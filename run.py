53c1370812d0631494d901c0913a3c9a78b3a259
Rommel Anatoli Quintanilla Cruz <romeluko@gmail.com>
2017-12-12 23:21:14 -0200

    Linking module nvidia-ml-py..

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+from subprocess import Popen, PIPE, STDOUT
+import os
 import sqlite3
 conn = sqlite3.connect('kernels.db')
 conn.row_factory = sqlite3.Row
@@ -5,6 +7,10 @@ cursor = conn.cursor()
 cursor.execute("SELECT * FROM Application;")
 
 for row in cursor.fetchall():
-    print(row["binary"],row["parameters"])
+    print(os.environ['RODINIA_DIR'],row["binary"],row["parameters"])
+    cmd = os.environ['RODINIA_DIR']+row["binary"]+" "+row["parameters"]
+    nvprof_cmd = os.environ["CUDA_DIR"]+"bin/nvprof -o output_"+row["title"].lower().replace(" ", "_")+".db" + " " + cmd
+    p = Popen(nvprof_cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=None,  close_fds=True)
+    print p.stdout.read()
 
 conn.close()
