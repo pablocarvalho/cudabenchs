dc125ea7f85337b6226509b8cc140dcc8e93949d
Rommel Anatoli Quintanilla Cruz <romeluko@gmail.com>
2017-12-13 03:38:43 -0200

    Fixing script errors...

--- run.py
+++ run.py
@@ -20,6 +20,7 @@ class ApplicationRunner:
     def __init__(self):
         self.connection = sqlite3.connect('kernels.db')
         self.connection.row_factory = sqlite3.Row
+        self.cursor = None
 
     def run(self):
         self.cursor = self.connection.cursor()
@@ -30,10 +31,10 @@ class ApplicationRunner:
         for row in self.cursor.fetchall():
             print(os.environ[row['environment']],row["binary"],row["parameters"])
             cmd = os.environ[row['environment']] + row["binary"] + " " + row["parameters"]
-            db_name = format_name(get_device_name(device_idx)) + "_" + format_name(row['name']) + " " + format_name(row["title"]) + ".db"
+            db_name = format_name(get_device_name(device_idx)) + "_" + format_name(row['name']) + "_" + format_name(row["title"]) + ".db"
             nvprof_cmd = os.environ["CUDA_DIR"] + "bin/nvprof -o " + db_name + " " + cmd
             p = Popen(nvprof_cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=None, close_fds=True)
-            db_list.append((db_name,row['App._id_']))
+            db_list.append((db_name,row['_id_']))
             print p.stdout.read()
 
         self.cursor.close()
@@ -46,13 +47,13 @@ class KernelStorage:
     def __init__(self):
         self.connection = sqlite3.connect('kernels.db')
         self.connection.row_factory = sqlite3.Row
+        self.cursor = None
 
     def save(self):
         runner = ApplicationRunner()
         db_list = runner.run()
 
         for (db,app_id) in db_list:
-            self.cursor.execute('select _id_ from Application where ')
             connection = sqlite3.connect(db)
             connection.row_factory = sqlite3.Row
             cursor = connection.cursor()
@@ -60,7 +61,11 @@ class KernelStorage:
 
             self.cursor = self.connection.cursor()
             for row in cursor.fetchall():
-                self.cursor.execute("insert into Kernels values (?,?,?,?,?,?,?,?,?,?,?,?)",(row['registersPerThread'],str(int(row['end'])-int(row['start'])),row['gridX'],row['gridY'],row['gridZ'],row['blockX'],row['blockY'],row['blockZ'],row['staticSharedMemory'],row['dynamicSharedMemory'],row['value'],app_id))
+                self.cursor.execute("insert into Kernels(registersPerThread,avgTime,gridX,gridY,gridZ,blockX,blockY,blockZ,staticSharedMemory,dynamicSharedMemory,name,application) values (?,?,?,?,?,?,?,?,?,?,?,?)",
+                                    (row['registersPerThread'],str(int(row['end'])-int(row['start'])),
+                                     row['gridX'],row['gridY'],row['gridZ'],row['blockX'],
+                                     row['blockY'],row['blockZ'],row['staticSharedMemory'],
+                                     row['dynamicSharedMemory'],row['value'],app_id))
                 self.connection.commit()
             self.cursor.close()
 
