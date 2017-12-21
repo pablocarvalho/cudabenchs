b0a3153dd254ae741091d45e4d1943050cb2d394
Rommel Anatoli Quintanilla Cruz <romeluko@gmail.com>
2017-12-21 05:15:01 -0200

    Adding db-name parameter...

--- db.py
+++ db.py
@@ -1,8 +1,17 @@
 import sqlite3
 
 class DBConnection:
-    def __init__(self):
-        self.connection = sqlite3.connect('kernels.db')
+    def __init__(self,db=None):
+
+        if db is None:
+            db = 'kernels.db'
+
+        try:
+            self.connection = sqlite3.connect(db)
+        except sqlite3.Error:
+            print "Error opening db: " + db
+            raise
+
         self.connection.row_factory = sqlite3.Row
         self.cursor = None
 
