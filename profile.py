9cccc1a4f49532d89f0f2bd392ce1ddce7c4acce
Rommel Anatoli Quintanilla Cruz <romeluko@gmail.com>
2018-01-01 21:58:29 -0200

    Updating profile script supporting the average calculation time..

--- profile.py
+++ profile.py
@@ -32,7 +32,7 @@ class ApplicationRunner(DBConnection):
             cur_path = os.getcwd()
             os.chdir(os.environ[row['environment']] + row["binary"][:-len(row["binary"].split('/')[-1])])
 
-            nvprof_cmd = os.environ["CUDA_DIR"] + "/bin/nvprof -o " + cur_path + "/" + db_name + " " + cmd
+            nvprof_cmd = os.environ["CUDA_DIR"] + "/bin/nvprof -f -o " + cur_path + "/" + db_name + " " + cmd
             log.info("Calling: "+nvprof_cmd)
             _env = os.environ.copy()
             _env['CUDA_VISIBLE_DEVICES'] = str(device)
@@ -45,55 +45,80 @@ class ApplicationRunner(DBConnection):
             if p.returncode: #or errors:
                 print errors
             else:
-                db_list.append((db_name,row['_id_']))
+                db_list.append((db_name, row['_id_']))
 
         self.cursor.close()
         return db_list
 
 class KernelStorage(DBConnection):
     def save(self):
-        runner = ApplicationRunner()
-        db_list = runner.run()
-        print "Loading "+str(len(db_list))+" database(s).."
+        num_iterations = 30
 
-        for (db,app_id) in db_list:
-            print "Connecting to DB: "+db
-            connection = sqlite3.connect(db)
-            connection.row_factory = sqlite3.Row
-            cursor = connection.cursor()
-            cursor.execute("""SELECT count(*) AS invocations,
-                                (end-start) AS actualTime,
-                                avg(end-start) AS avgTime,
-                                min(end-start) AS minTime,
-                                max(end-start) AS maxTime,
-                                *
-                            FROM CUPTI_ACTIVITY_KIND_CONCURRENT_KERNEL AS CK INNER JOIN StringTable AS ST 
-                            WHERE ST._id_=CK.name GROUP BY value;""")
+        for num_iteration in range(num_iterations):
+            print highlight_str("#Iteration: " + str(num_iteration))
 
-            self.cursor = self.connection.cursor()
-            rows = cursor.fetchall()
-            print highlight_str("Loading "+str(len(rows))+" kernel(s)..")
+            runner = ApplicationRunner()
+            db_list = runner.run()
+            print "Loading " + str(len(db_list)) + " database(s).."
 
-            for row in rows:
-                try:
-                    self.cursor.execute("""INSERT INTO Kernels(registersPerThread,actualTime,
-                                            invocations,avgTime,minTime,maxTime,gridX,gridY,gridZ,
-                                            blockX,blockY,blockZ,staticSharedMemory,dynamicSharedMemory,
-                                            mangledName,application)
-                                        VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)""",
-                                        (row['registersPerThread'], row['actualTime'],
-                                         row['invocations'], row['avgTime'],
-                                         row['minTime'], row['maxTime'],
-                                         row['gridX'], row['gridY'], row['gridZ'], row['blockX'],
-                                         row['blockY'], row['blockZ'], row['staticSharedMemory'],
-                                         row['dynamicSharedMemory'], row['value'], app_id))
+            for (db, app_id) in db_list:
+                print "Connecting to DB: " + db
+                connection = sqlite3.connect(db)
+                connection.row_factory = sqlite3.Row
+                cursor = connection.cursor()
+                cursor.execute("""SELECT count(*) AS invocations,
+                                    (end-start) AS actualTime,
+                                    avg(end-start) AS avgTimeApp,
+                                    min(end-start) AS minTimeApp,
+                                    max(end-start) AS maxTimeApp,
+                                    *
+                                FROM CUPTI_ACTIVITY_KIND_CONCURRENT_KERNEL AS CK INNER JOIN StringTable AS ST 
+                                WHERE ST._id_=CK.name GROUP BY value;""")
+
+                nvprof_rows = cursor.fetchall()
+
+                print highlight_str("Loading " + str(len(nvprof_rows)) + " kernel(s)..")
+
+                for nvprof_row in nvprof_rows:
+                    self.cursor = self.connection.cursor()
+                    self.cursor.execute("SELECT _id_ FROM Kernels WHERE mangledName = ?;", (nvprof_row['value'],))
+                    app_rows = self.cursor.fetchall()
+
+                    if(len(app_rows) == 0):
+                        self.cursor.execute("""INSERT INTO Kernels(registersPerThread,actualTime,
+                                                    invocations,avgTimeApp,minTimeApp,maxTimeApp,gridX,gridY,gridZ,
+                                                    blockX,blockY,blockZ,staticSharedMemory,dynamicSharedMemory,
+                                                    mangledName,application)
+                                                VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)""",
+                                                (nvprof_row['registersPerThread'], nvprof_row['actualTime'],
+                                                nvprof_row['invocations'], nvprof_row['avgTime'],
+                                                nvprof_row['minTime'], nvprof_row['maxTime'],
+                                                nvprof_row['gridX'], nvprof_row['gridY'], nvprof_row['gridZ'], nvprof_row['blockX'],
+                                                nvprof_row['blockY'], nvprof_row['blockZ'], nvprof_row['staticSharedMemory'],
+                                                nvprof_row['dynamicSharedMemory'], nvprof_row['value'], app_id))
+                        self.connection.commit()
+
+                    self.cursor.execute("SELECT * FROM Kernels WHERE mangledName = ?", (nvprof_row['value'],))
+                    old_target_row = self.cursor.fetchone()
+
+                    cursor.execute("""SELECT (end-start) AS actualTime,* FROM CUPTI_ACTIVITY_KIND_CONCURRENT_KERNEL
+                                    AS CK INNER JOIN StringTable AS ST 
+                                    WHERE ST._id_=CK.name and value = ?;""", (nvprof_row['value'],))
+                
+                    target_row = cursor.fetchone()
+
+                    self.cursor.execute("""UPDATE Kernels SET minTimeFirst = ?,
+                                            maxTimeFirst = ?, avgTimeFirst = ?
+                                            WHERE mangledName = ?;""",
+                                        (min(old_target_row['minTimeFirst'] if old_target_row['minTimeFirst'] is not None else float('+inf'), target_row['actualTime']),
+                                        max(old_target_row['maxTimeFirst'] if old_target_row['maxTimeFirst'] is not None else float('-inf'), target_row['actualTime']),
+                                        int(old_target_row['avgTimeFirst'] if old_target_row['avgTimeFirst'] is not None else 0) + int(target_row['actualTime'])/float(num_iterations),
+                                        nvprof_row['value']))
                     self.connection.commit()
-                except sqlite3.Error as er:
-                    log.warning(er.message + ", kernel" + row['value'])
-            self.cursor.close()
+                self.cursor.close()
 
-            cursor.close()
-            connection.close()
+                cursor.close()
+                connection.close()
 
 storage = KernelStorage()
 storage.save()
