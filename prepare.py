9fb855f542ff6d5afbcd4e7ad6f911e8deff5c7f
Rommel A Q Cruz <romeluko@gmail.com>
2018-03-23 14:53:37 -0300

    Adding preparation script...

---
+++ prepare.py
@@ -0,0 +1,15 @@
+from db import DBConnection
+
+db2 = 'kernels.db'
+db1 = 'kernels-gtx_980-30its.db'
+conn1 = DBConnection(db1)
+conn2 = DBConnection(db2)
+
+cursor1 = conn1.connection.cursor()
+cursor2 = conn2.connection.cursor()
+
+cursor1.execute("""select * from kernels;""")
+result1 = cursor1.fetchall()
+
+for row1 in result1:
+	cursor2.execute("""update kernels set codeName = ? where mangledName = ?""",row1['codeName'],row1['mangledName']);	
